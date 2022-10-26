84a57b47e01a42ec1e099a96f1c67d85e367f798
sayali-2001 <sayalithakare47@gmail.com>
2022-10-26 13:14:57 +0530

    Initial Commit

--- ClassA.java
+++ ClassA.java
@@ -2,6 +2,6 @@ package bridgelabz;
 
 public class ClassA {
     public static void main(String[] args) {
-        System.out.println("User Registration Problem");
+        System.out.println("Mood Analyzer");
     }
 }
